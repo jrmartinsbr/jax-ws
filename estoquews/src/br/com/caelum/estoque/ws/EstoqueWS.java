4544e5f089b5381d9958ec2c84793784a6419823
80197616 <jrmartinsj@indracompany.com>
2020-10-05 12:17:29 -0300

    Atualizando filtros

--- estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -6,9 +6,11 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.ResponseWrapper;
 
 import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Filtros;
+import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ListaItens;
 
@@ -17,13 +19,21 @@ public class EstoqueWS {
 	
 	ItemDao dao = new ItemDao();
 	
-	@WebMethod(operationName = "todosOsItens")
+	@WebMethod(operationName = "ConsultarItens")
 	@WebResult(name = "itens")
+	@ResponseWrapper(localName = "retornoConsultarItens")
 	public ListaItens getItens(@WebParam(name = "filtros") Filtros filtros) {
 		List<Filtro> lista = filtros.getLista();
 		System.out.println("Chamando todosItens()");
 		ListaItens listaItens = new ListaItens(dao.todosItens(lista));
 		return listaItens;
+	}
+	
+	@WebMethod(operationName = "CadastrarItem")
+	@WebResult(name = "item")
+	public Item cadastrarItem(@WebParam(name = "item") Item item) {
+		this.dao.cadastrar(item);
+		return item;
 	}
 
 }
